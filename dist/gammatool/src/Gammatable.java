2f7b0ca699c45952f40f396e02ad5cd16fb27993
Hauke Petersen <mail@haukepetersen.de>
2014-01-11 18:13:33 +0100

    Fixes to the Gammatable generator

--- dist/gammatool/src/Gammatable.java
+++ dist/gammatool/src/Gammatable.java
@@ -9,7 +9,7 @@ public class Gammatable {
 	private int[] table = null;
 	private int numOfValues = 0;
 	private int maxValue = 0;
-	private int numOfRows = 0;
+	private int numOfCols = 0;
 	BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
 	
 	
@@ -19,15 +19,15 @@ public class Gammatable {
 	public void askParams() {
 		String input;
 		try{
-			System.out.print("Number of Values: ");
+			System.out.print("Number of Values (different color steps): ");
 			input = bufferRead.readLine();
 			numOfValues = Integer.parseInt(input);
-			System.out.print("Maximum Compare Value: ");
+			System.out.print("Maximum Compare Value (maximum pwm period): ");
 			input = bufferRead.readLine();
 			maxValue = Integer.parseInt(input);
-			System.out.print("Number of ouput lines: ");
+			System.out.print("Number of ouput columns: ");
 			input = bufferRead.readLine();
-			numOfRows = Integer.parseInt(input);
+			numOfCols = Integer.parseInt(input);
 		}
 		catch(IOException e)
 		{
@@ -36,31 +36,32 @@ public class Gammatable {
 	}
 	
 	public void calculate() {
-		table = new int[numOfValues + 1];
-		for (int i = 0; i <= numOfValues; i++) {
+		table = new int[numOfValues];
+		for (int i = 1; i <= numOfValues; i++) {
 			double max = maxValue;
 			double quot = (double)i / numOfValues;
 			double res = Math.pow(max, quot);
-			table[i] = (int)Math.round(res);
+			table[i - 1] = (int)Math.round(res);
 		}
 	}
 	
 	public void print() {
-		int rowcount = 0;
-		System.out.println("Results: ");
-		for (int i = 0; i <= numOfValues; ) {
+        int rowcount = 0;
+		System.out.println("Results: \n");
+		for (int i = 0; i < numOfValues; ) {
 
-			String row = "\t";
-			for (int j = 0; j < numOfRows && i <= numOfValues; j++) {
-				row += "\t" + stuff(table[i++]) + ",";
+			String row = "";
+			for (int j = 0; j < numOfCols && i < numOfValues; j++) {
+                if (j > 0) {
+                    row += "\t";
+                }
+				row += stuff(table[i++]) + ",";
 			}
 			System.out.println(row);
 			rowcount++;
 		}
 		
-		System.out.println("Generated a table with " + table.length + " entries. Rowcount: " + rowcount);
-		System.out.println("Entry 1000: " + table[1000]);
-		
+		System.out.println("\nGenerated a table with " + table.length + " entries. Rowcount: " + rowcount);
 	}
 	
 	
