cbfd4892b6a01d7b306a6ecc37a4f08f3151593b
Hauke Petersen <mail@haukepetersen.de>
2014-01-16 14:06:49 +0100

    Added some firmware documentation

---
+++ firmware/doc/templates/template.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2014 Hauke Petersen <mail@haukepetersen.de>
+ *
+ * This file is subject to the terms and conditions of the GPLv2 License. 
+ * See the file LICENSE in the top level directory for more details.
+ */
+
+/**
+ * @ingroup     UNDEF
+ * @{
+ * 
+ * @file        FILENAME
+ * @brief       DESCRIPTION
+ *
+ * @author      Hauke Petersen <mail@haukepetersen.de>
+ */
+
+#ifndef __FILENAME_H
+#define __FILENAME_H
+
+
+
+#endif /* __FILENAME_H */
+/** @} */
